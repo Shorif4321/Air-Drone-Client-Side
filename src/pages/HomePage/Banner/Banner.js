81936555ed16273be0cec607b990f57ad2231423
Shorif4321 <shorifali4321@gmail.com>
2022-01-19 00:06:05 +0600

    first commit

---
+++ src/pages/HomePage/Banner/Banner.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Banner.css";
+
+const Banner = () => {
+    return (
+        <div className="banner-img">
+            <h1>
+                "Drones overall will be more impactful than I think people
+                recognize, in positive ways to help society."
+            </h1>
+        </div>
+    );
+};
+
+export default Banner;
